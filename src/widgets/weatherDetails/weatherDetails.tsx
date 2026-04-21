dfa0eca96242370ca37a00e19e0df3bbebc3d6d8
Yaroslava <yaroslava.ger4@gmail.com>
2026-04-21 18:43:57 +0400

    added forecast every 3 hours

--- src/widgets/weatherDetails/weatherDetails.tsx
+++ src/widgets/weatherDetails/weatherDetails.tsx
@@ -111,14 +111,14 @@ function WeatherDetails() {
 
 
 
- <div><p>Sunrise</p> <span>time</span></div>
+ <div className="weather-details__astro-info"><p className="weather-details__astro-title">Sunrise</p> <span className="weather-details__astro-time">{data.forecast.forecastday[0].astro.sunrise}</span></div>
 </div>
 
 <div className="weather-details__astro">
 
 <svg xmlns="http://www.w3.org/2000/svg" width="84" height="84" viewBox="0 0 512 512"><defs><linearGradient id="SVGeq4GoeLw" x1="150" x2="234" y1="119.2" y2="264.8" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fbbf24"/><stop offset=".5" stop-color="#fbbf24"/><stop offset="1" stop-color="#f59e0b"/></linearGradient><clipPath id="SVGzpDhEbnf"><path fill="none" d="M512 306H296a21.5 21.5 0 0 0-14 5.3L256 334l-26-22.7a21.5 21.5 0 0 0-14-5.3H0V0h512Z"/></clipPath><symbol id="SVG0a04Kbxn" viewBox="0 0 384 384"><circle cx="192" cy="192" r="84" fill="url(#SVGeq4GoeLw)" stroke="#f8af18" stroke-miterlimit="10" stroke-width="6"/><path fill="none" stroke="#fbbf24" stroke-linecap="round" stroke-miterlimit="10" stroke-width="24" d="M192 61.7V12m0 360v-49.7m92.2-222.5l35-35M64.8 319.2l35.1-35.1m0-184.4l-35-35m254.5 254.5l-35.1-35.1M61.7 192H12m360 0h-49.7"><animateTransform additive="sum" attributeName="transform" dur="6s" repeatCount="indefinite" type="rotate" values="0 192 192; 45 192 192"/></path></symbol></defs><g clip-path="url(#SVGzpDhEbnf)"><use width="384" height="384" href="#SVG0a04Kbxn" transform="translate(64 100)"/></g><path fill="none" stroke="#374151" stroke-linecap="round" stroke-linejoin="round" stroke-width="18" d="M128 332h88l40 36l40-36h88"/></svg>
 
- <div><p>Sunset</p> <span>time</span></div>
+ <div className="weather-details__astro-info"><p className="weather-details__astro-title">Sunset</p> <span className="weather-details__astro-time">{data.forecast.forecastday[0].astro.sunset}</span></div>
 
 </div>
 
--- src/widgets/weatherForecast/weatherForecast.tsx
+++ src/widgets/weatherForecast/weatherForecast.tsx
@@ -1,12 +1,22 @@
+import { useWeatherStore } from "../../features/search/modal/store"
 import "./weatherForecast.scss"
 function WeatherForecast(){
+    const data=useWeatherStore(state=>state.data)
+  if (!data || !data.forecast)return null  
+const forecast=data.forecast.forecastday[0].hour
 
+const filtredTime= forecast.filter((_, index)=>  index %3===0)
+console.log(filtredTime)
     return(
         <section className="weather-forecast__wrapper">
             <h3>Forecast</h3>
-            <div>
-
-            элементы
+            <div  className="weather-forecast__time-wrapper">
+{filtredTime.map(time => (
+<div className="weather-forecast__info"><p>{time.time}</p>
+<img src={time.condition.icon} />
+<p>{Math.round(time.temp_c)}&deg;C</p>
+</div>
+))}
             </div>
         </section>
     )
